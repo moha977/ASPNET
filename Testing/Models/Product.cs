a42a6928481748545164bd12d15256707d4dba11
Mohammad ahma <ahmad.moh0812@icloud.com>
2022-02-15 11:20:19 -0500

    Finished

---
+++ Testing/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Testing.Models;
+
+namespace Testing
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+        public int ProductID { get; set; }  
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int CategoryID {get; set; }
+        public int OnSale { get; set; } 
+        public int StockLevel { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
